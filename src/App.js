2daee0f06782a0e232662f8fe56a31d7f39f1de4
Yash Rajgor <yash592@gmail.com>
2018-01-08 22:48:49 -0800

    characters

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import './App.css';
 import Jumbotron from "./components/jumbo";
 import Navbar from "./components/nav";
 import Social from "./components/social";
+import characters from "./characters.json";
 
 class App extends Component {
+
+  // Seting the characters array 
+  state = {
+    characters
+  };
+
+
   render() {
     return (
       <div className="App">
