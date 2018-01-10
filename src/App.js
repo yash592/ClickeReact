f7239df994013a8c3e9854b1b3bad7ec13095beb
Yash Rajgor <yash592@gmail.com>
2018-01-09 20:22:28 -0800

    Logic

--- src/App.js
+++ src/App.js
@@ -7,12 +7,42 @@ import Social from "./components/social";
 import characters from "./characters.json";
 import CharacterCard from "./components/CharacterCard";
 
+let array = [1, 2, 3];
+console.log(array)
 class App extends Component {
 
   // Seting the characters array 
   state = {
-    characters
+    characters,
+    score: "",
+    topScore: "",
+    clicked: false
   };
+
+
+
+  componentDidMount = () => {
+    this.shuffleChars();
+
+  }
+
+
+
+  shuffleChars = () => {
+
+    var counter = array.length, temp, index;
+    while (counter> 0) {
+      index = Math.floor(Math.random()*counter);
+      counter--;
+      temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+    }
+
+    console.log(array);
+    console.log(this.state.characters);
+
+  }
 
 
   render() {
